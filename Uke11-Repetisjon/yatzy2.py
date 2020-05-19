724cc81e2fb833da1c713d1e98455e9254a6e1cb
Einar Høst <Einar.Host@nrk.no>
2020-05-19 14:56:17 +0200

    Mer yatzy.

--- Uke11-Repetisjon/yatzy2.py
+++ Uke11-Repetisjon/yatzy2.py
@@ -33,6 +33,31 @@ def beregn_kast_direkte(et_kast, terningverdi):
 #(verdi, terninger_igjen) = beregn_kast([2, 5, 5, 2, 4], 2)
 (verdi, terninger_igjen) = beregn_kast_direkte([2, 5, 5, 2, 4], 2)
 
-print(f"Verdi av 2'ere: {verdi}")
-print(f"Terninger igjen: {terninger_igjen}")
+def runde(terningverdi):
+    print(f"Kaster en runde for å få terninger med verdi {terningverdi}")
+    totalverdi = 0
+    terninger = 5
+    for kast_nr in range(0, 3):
+        print(f"Kast nummer {kast_nr + 1} (med {terninger} terninger).")
+        et_kast = kast(terninger)
+        print(f"Kastet ble: {et_kast}")
+        (verdi, terninger_igjen) = beregn_kast(et_kast, terningverdi)
+        print(f"Kastet var verdt: {verdi}")
+        print(f"Jeg har igjen {terninger_igjen} terninger.")
+        totalverdi = totalverdi + verdi
+        terninger = terninger_igjen
+    return totalverdi
 
+enere = runde(1)
+toere = runde(2)
+treere = runde(3)
+firere = runde(4)
+femmere = runde(5)
+seksere = runde(6)
+
+totalt = enere + toere + treere + firere + femmere + seksere 
+print(f"Du fikk {totalt} i sum.")
+if totalt >= 63:
+    print("Gratulerer, du fikk bonus!")
+else:
+    print("Dessverre, det ble ingen bonus på deg.")
