14832483d17197ce62ead7705e6f94c16ba3fbb2
Einar Høst <Einar.Host@nrk.no>
2020-05-19 13:40:38 +0200

    Update eksempler.py

--- Uke11-Repetisjon/eksempler.py
+++ Uke11-Repetisjon/eksempler.py
@@ -11,8 +11,22 @@
 # * en_mer(1) => 2
 # * en_mer(2) => 3
 # * en_mer(-1) => 0
+
+def en_mer(n):
+    return n + 1
+
+tall = 0
+svar = en_mer(tall)
+
+
 #
 # Hvordan kan du bruke funksjonen `en_mer` til å lage et tall som er 3 større? 5 større?
+
+tall = 0
+svar = en_mer(en_mer(en_mer(0)))
+
+#print(f"{svar} er tre mer enn {tall}")
+
 
 ##########################################################################################
 
@@ -26,10 +40,34 @@
 # * dobbel(0) => 0
 # * dobbel(1) => 2
 # * dobbel(2) => 4
-# * dobbel(3) => 3
+# * dobbel(3) => 6
+
+def dobbel(n):
+    return n + n
+
+def dobbel_2(n):
+    return 2 * n
+
+tall = 3
+svar = dobbel(tall)
+
+#print(f"{svar} er det dobbelte av {tall}")
 
 # Kan du implementere `dobbel_med_en_mer` som gir samme svar som `dobbel`, 
 # men bruker funksjonen `en_mer` istedenfor addition eller multiplikasjon?
+
+def dobbel_med_en_mer(n):
+    svar = n
+    for i in range(0, n):
+        svar = en_mer(svar)
+    return svar
+
+tall = 4
+svar = dobbel_med_en_mer(tall)
+
+#print(f"{svar} er det dobbelte av {tall}")
+
+
 
 ##########################################################################################
 
@@ -40,10 +78,19 @@
 
 # Noen eksempler: 
 # 
-# * bare_positive([1, 0, -34, 278, 7]) => []
+# * bare_positive([1, 0, -34, 278, 7]) => [1, 278, 7]
 # * bare_positive([1, 2, 3]) => [1, 2, 3]
 # * bare_positive([-1, -2, -3]) => []
 # * bare_positive([0, 1, -2, 3]) => [1, 3]
+
+def bare_positive(liste):
+    svar = []
+    for tall in liste:
+        if tall > 0:
+            svar.append(tall)
+    return svar
+
+print(bare_positive([0, 1, -2, 3]))
 
 ##########################################################################################
 
@@ -82,8 +129,14 @@ programliste = [
     }
 ]
 
-nrk2_titler = []
-# nrk2_titler = velg_programmer_for_kanal(programliste, "NRK2")
+def velg_programmer_for_kanal(programmer, kanal):
+    svar = []
+    for p in programmer:
+        if p["kanal"] == kanal:
+            svar.append(p["tittel"])
+    return svar
+
+nrk2_titler = velg_programmer_for_kanal(programliste, "NRK2")
 
 print(nrk2_titler)
 
